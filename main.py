e2436ec7f21e69b77db9d287aeb69e6509395535
Jumpei-Fujita <jumpei06240808@gmail.com>
2021-08-20 09:28:05 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from model import RecipeBART
+from transformers import AutoTokenizer
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base")
+
+st.title('Automatic Recipe Generation')
+st.write('自動料理手順生成')
+data_load_state = st.text('loading....（準備中）')
+model = RecipeBART()
+model.load_state_dict(torch.load('recipe_bart_cpu.pth'))
+data_load_state = st.text('ready to start!!（準備完了！！）')
+
+st.sidebar.write('sampling parameter(サンプリング手法)')
+tmp = st.sidebar.slider("tmperature", min_value=0.5, max_value=1.0, step=0.01, value=1.0)
+top_k = st.sidebar.slider("top-k", min_value=2, max_value=100, step=1, value=40)
+
+left_column, right_column = st.columns(2)
+name = left_column.text_input('cooking name(料理名)', 'vegetable soup meatloaf')
+ingredients = right_column.text_input('ingredients you want to use(利用したい材料)', 'onion,salt')
+right_column.caption('Enter the ingredients separated by ,(,で区切って入力してください)')
+
+make_recipe_button = st.button('make recipe(料理手順生成)')
+print(name)
+print(ingredients)
+st.components.v1.html('<a href="https://www.google.com/" class="btn">サンプル</a>')
+
+if make_recipe_button:
+    if (ingredients.replace(' ','') != '') and (name.replace(' ','') != ''):
+        data_load_state = st.text('Loading data...(頑張って作っています)')
+        ingredients = ingredients.replace(', ', ',').replace(' ,', ',')
+        recipe = 'aa'
+        n = 0
+        while len(recipe.replace('<s>', '').replace('</s>', '').split('>')) != 2:
+            recipe = tokenizer.decode(model.generate(name, ingredients, False, top_k=top_k, tmp=tmp))
+            n += 1
+            if n > 4:
+                st.write('please try again(もう一度お試しください)')
+                break
+        recipe = recipe.replace('<s>', '').replace('</s>', '').split('>')
+        if len(recipe) == 2:
+            data_load_state = st.text('Done!!(できた！！)')
+            ingr = ', '.join(list(set(recipe[0].split(','))))
+            step = recipe[1]
+            st.subheader('required ingredients（必要材料）')
+            st.write(ingr)
+            st.subheader('cooking steps（料理手順）')
+            st.write(step)
+            
+    else:
+        st.write('Please fill in the blanks.(空欄を埋めてください)')
---
+++ main2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from model import RecipeBART
+from transformers import AutoTokenizer
+import torch
+
+st.components.v1.html('<a href="https://www.google.com/" class="btn">サンプル</a>')
